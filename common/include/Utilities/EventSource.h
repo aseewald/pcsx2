aseewald/pcsx2

commit 32fa179fd0cb3e9eaf09ed027f26367b50c51337

Reorganized the exception/signal handlers, setjmp/longjmp, and SysCoreThread stuff:  * Exception/Signal handling now uses an EventSource, so that multiple handlers can be registered.  This is in preparation for (eventual) more complete MIPS TLB support in the VTLB memory model.  * Improved code isolation, so that recompiler-specific code is primarily in iR5900-32.cpp (cleans up Counters.cpp and SysCoreThread.cpp)
git-svn-id: http://pcsx2.googlecode.com/svn/trunk@2063 96395faa-99c1-11dd-bbfe-3dabce05a288

common/include/Utilities/EventSource.h
@@ -16,12 +16,17 @@
 #pragma once
 
 #include <list>
-#include <wx/event.h>
+
+class wxCommandEvent;
+
+// --------------------------------------------------------------------------------------
+//  EventListener< typename EvtType >
+// --------------------------------------------------------------------------------------
 
 template< typename EvtType >
 struct EventListener
 {
-	typedef void FuncType( void* object, const EvtType& evt );
+	typedef void __fastcall FuncType( void* object, EvtType& evt );
 
 	void*		object;
 	FuncType*	OnEvent;
@@ -49,6 +54,10 @@ struct EventListener
 	}
 };
 
+// --------------------------------------------------------------------------------------
+//  EventSource< template EvtType >
+// --------------------------------------------------------------------------------------
+
 template< typename EvtType >
 class EventSource
 {
@@ -58,23 +67,38 @@ public:
 	typedef typename ListenerList::iterator			Handle;
 
 protected:
+	typedef typename ListenerList::const_iterator	ConstIterator;
+
 	ListenerList m_listeners;
+	
+	// This is a cached copy of the listener list used to handle standard dispatching, which
+	// allows for self-modification of the EventSource's listener list by the listeners.
+	// Translation: The dispatcher uses this copy instead, to avoid iterator invalidation.
+	ListenerList	m_cache_copy;
+	bool			m_cache_valid;
 
 public:
+	EventSource() : m_cache_valid( false )
+	{
+	}
+
 	virtual ~EventSource() throw() {}
 
 	virtual void Remove( const ListenerType& listener )
 	{
+		m_cache_valid = false;
 		m_listeners.remove( listener );
 	}
 
 	virtual void Remove( const Handle& listenerHandle )
 	{
+		m_cache_valid = false;
 		m_listeners.erase( listenerHandle );
 	}
 
 	virtual Handle AddFast( const ListenerType& listener )
 	{
+		m_cache_valid = false;
 		m_listeners.push_front( listener );
 		return m_listeners.begin();
 	}
@@ -93,14 +117,21 @@ public:
 		Remove( ListenerType( objhandle, fnptr ) );
 	}
 
-	void Dispatch( const EvtType& evt ) const
+	void Dispatch( EvtType& evt )
 	{
-		// Have to make a complete copy of the list, because the event stack can change:
-		
-		ListenerList list( m_listeners );
-		
-		typename ListenerList::const_iterator iter = list.begin();
-		while( iter != list.end() )
+		if( !m_cache_valid )
+		{
+			m_cache_copy = m_listeners;
+			m_cache_valid = true;
+		}
+
+		_DispatchRaw( m_cache_copy.begin(), m_cache_copy.end(), evt );
+	}
+
+protected:
+	__forceinline void _DispatchRaw( ConstIterator& iter, const ConstIterator& iend, EvtType& evt )
+	{
+		while( iter != iend )
 		{
 			try
 			{
@@ -118,14 +149,15 @@ public:
 			++iter;
 		}
 	}
+
 };
 
 // --------------------------------------------------------------------------------------
-//  EventListenerBinding
+//  EventListenerBinding< typename EvtType ?
 // --------------------------------------------------------------------------------------
 // Encapsulated event listener binding, provides the "benefits" of object unwinding.
 //
-template< typename EvtType = wxCommandEvent >
+template< typename EvtType >
 class EventListenerBinding
 {
 public:
@@ -211,9 +243,7 @@ public:
 template< typename EvtType >
 void EventSource<EvtType>::RemoveObject( const void* object )
 {
-	// Iso C++ rules regarding temporaries, specifically that non-const temporaries are disallowed,
-	// also removes any actual convenience factor that unary predicates may have actually offered. >_<
-
+	m_cache_valid = false;
 	m_listeners.remove_if( PredicatesAreTheThingsOfNightmares<EvtType>( object ) );
 }
 

pcsx2/Memory.cpp
@@ -48,7 +48,7 @@ BIOS
 #include "VUmicro.h"
 #include "GS.h"
 #include "IPU/IPU.h"
-#include "AppConfig.h"
+#include "System/PageFaultSource.h"
 
 
 #ifdef ENABLECACHE
@@ -574,6 +574,8 @@ void memClearPageAddr(u32 vaddr)
 ///////////////////////////////////////////////////////////////////////////
 // PS2 Memory Init / Reset / Shutdown
 
+static void __fastcall mmap_OnPageFault( void* basemem, PageFaultInfo& info );
+
 static const uint m_allMemSize =
 		Ps2MemSize::Rom + Ps2MemSize::Rom1 + Ps2MemSize::Rom2 + Ps2MemSize::ERom +
 		Ps2MemSize::Base + Ps2MemSize::Hardware + Ps2MemSize::Scratch;
@@ -596,10 +598,14 @@ void memAlloc()
 	psER = curpos; curpos += Ps2MemSize::ERom;
 	psH = curpos; curpos += Ps2MemSize::Hardware;
 	psS = curpos; //curpos += Ps2MemSize::Scratch;
+	
+	Source_PageFault.Add( EventListener<PageFaultInfo>((void*)psM, mmap_OnPageFault) );
 }
 
 void memShutdown()
 {
+	Source_PageFault.Remove( EventListener<PageFaultInfo>((void*)psM, mmap_OnPageFault) );
+
 	vtlb_free( m_psAllMem, m_allMemSize );
 	m_psAllMem = NULL;
 	psM = psR = psR1 = psR2 = psER = psS = psH = NULL;
@@ -623,8 +629,7 @@ void memBindConditionalHandlers()
 void memReset()
 {
 	// VTLB Protection Preparations.
-
-	HostSys::MemProtect( m_psAllMem, m_allMemSize, Protect_ReadWrite );
+	//HostSys::MemProtect( m_psAllMem, m_allMemSize, Protect_ReadWrite );
 
 	// Note!!  Ideally the vtlb should only be initialized once, and then subsequent
 	// resets of the system hardware would only clear vtlb mappings, but since the
@@ -874,28 +879,33 @@ void mmap_MarkCountedRamPage( u32 paddr )
 	HostSys::MemProtect( &psM[rampage<<12], 1, Protect_ReadOnly );
 }
 
-// offset - offset of address relative to psM.  The exception handler for the platform/host
-// OS should ensure that only addresses within psM address space are passed.   Anything else
-// will produce undefined results (ie, crashes).
-void mmap_ClearCpuBlock( uint offset )
+// offset - offset of address relative to psM.
+static __forceinline void mmap_ClearCpuBlock( uint offset )
 {
 	int rampage = offset >> 12;
 
 	// Assertion: This function should never be run on a block that's already under
 	// manual protection.  Indicates a logic error in the recompiler or protection code.
-	jASSUME( m_PageProtectInfo[rampage].Mode != ProtMode_Manual );
-
-	//#ifndef __LINUX__		// this function is called from the signal handler
-	//DbgCon.WriteLn( "Manual page @ 0x%05x", m_PageProtectInfo[rampage].ReverseRamMap>>12 );
-	//#endif
+	pxAssertMsg( m_PageProtectInfo[rampage].Mode != ProtMode_Manual,
+		"Attempted to clear a block that is already under manual protection." );
 
 	HostSys::MemProtect( &psM[rampage<<12], 1, Protect_ReadWrite );
 	m_PageProtectInfo[rampage].Mode = ProtMode_Manual;
 	Cpu->Clear( m_PageProtectInfo[rampage].ReverseRamMap, 0x400 );
 }
 
+static void __fastcall mmap_OnPageFault( void* basemem, PageFaultInfo& info )
+{
+	// get bad virtual address
+	uptr offset = info.addr - (uptr)basemem;
+	if( offset >= Ps2MemSize::Base ) return;
+
+	mmap_ClearCpuBlock( offset );
+	info.handled = true;
+}
+
 // Clears all block tracking statuses, manual protection flags, and write protection.
-// This does not clear any recompiler blocks.  IT is assumed (and necessary) for the caller
+// This does not clear any recompiler blocks.  It is assumed (and necessary) for the caller
 // to ensure the EErec is also reset in conjunction with calling this function.
 void mmap_ResetBlockTracking()
 {

pcsx2/RecoverySystem.cpp
@@ -34,7 +34,7 @@ int sys_resume_lock = 0;
 
 static FnType_OnThreadComplete* Callback_FreezeFinished = NULL;
 
-static void StateThread_OnAppStatus( void* thr, const enum AppEventType& stat )
+static void __fastcall StateThread_OnAppStatus( void* thr, AppEventType& stat )
 {
 	if( (thr == NULL) || (stat != AppStatus_Exiting) ) return;
 	((PersistentThread*)thr)->Cancel();

pcsx2/gui/AppMain.cpp
@@ -210,8 +210,7 @@ void Pcsx2App::OnEmuKeyDown( wxKeyEvent& evt )
 
 void Pcsx2App::HandleEvent(wxEvtHandler *handler, wxEventFunction func, wxEvent& event) const
 {
-	try
-	{
+	try {
 		(handler->*func)(event);
 	}
 	// ----------------------------------------------------------------------------
@@ -247,7 +246,7 @@ void Pcsx2App::HandleEvent(wxEvtHandler *handler, wxEventFunction func, wxEvent&
 	}
 }
 
-static void OnStateSaveFinished( void* obj, const wxCommandEvent& evt )
+static void __fastcall OnStateSaveFinished( void* obj, wxCommandEvent& evt )
 {
 	if( evt.GetInt() == CoreStatus_Resumed )
 	{
@@ -290,7 +289,8 @@ bool Pcsx2App::PrepForExit( bool canCancel )
 		}
 	}*/
 
-	m_evtsrc_AppStatus.Dispatch( AppStatus_Exiting );
+	AppEventType toSend = AppStatus_Exiting;
+	m_evtsrc_AppStatus.Dispatch( toSend );
 	CleanupMess();
 
 	return true;
@@ -354,7 +354,8 @@ void AppApplySettings( const AppConfig* oldconf, bool saveOnSuccess )
 		}
 	}
 
-	sApp.Source_SettingsApplied().Dispatch( 0 );
+	int toSend = 0;
+	sApp.Source_SettingsApplied().Dispatch( toSend );
 	CoreThread.ApplySettings( g_Conf->EmuOptions );
 	
 	if( resume )

pcsx2/gui/MainFrame.cpp
@@ -255,34 +255,33 @@ void MainEmuFrame::InitLogBoxPosition( AppConfig::ConsoleLogOptions& conf )
 	}
 }
 
-void MainEmuFrame::OnCoreThreadStatusChanged( void* obj, const wxCommandEvent& evt )
+void __fastcall MainEmuFrame::OnCoreThreadStatusChanged( void* obj, wxCommandEvent& evt )
 {
 	if( obj == NULL ) return;
 	MainEmuFrame* mframe = (MainEmuFrame*)obj;
 	mframe->ApplyCoreStatus();
 }
 
-void MainEmuFrame::OnCorePluginStatusChanged( void* obj, const wxCommandEvent& evt )
+void __fastcall MainEmuFrame::OnCorePluginStatusChanged( void* obj, wxCommandEvent& evt )
 {
 	if( obj == NULL ) return;
 	MainEmuFrame* mframe = (MainEmuFrame*)obj;
 	mframe->ApplyCoreStatus();
 }
 
-void MainEmuFrame::OnSettingsApplied( void* obj, const int& evt )
+void __fastcall MainEmuFrame::OnSettingsApplied( void* obj, int& evt )
 {
 	if( obj == NULL ) return;
 	MainEmuFrame* mframe = (MainEmuFrame*)obj;
 	mframe->ApplySettings();
 }
 
-void MainEmuFrame::OnSettingsLoadSave( void* obj, const IniInterface& evt )
+void __fastcall MainEmuFrame::OnSettingsLoadSave( void* obj, IniInterface& evt )
 {
 	if( obj == NULL ) return;
 	MainEmuFrame* mframe = (MainEmuFrame*)obj;
 
-	// FIXME: Evil const cast hack!
-	mframe->LoadSaveRecentIsoList( const_cast<IniInterface&>(evt) );
+	mframe->LoadSaveRecentIsoList( evt );
 }
 
 // ------------------------------------------------------------------------

pcsx2/gui/MainFrame.h
@@ -85,10 +85,10 @@ public:
 	void ReloadRecentLists();
 
 protected:
-	static void OnCoreThreadStatusChanged( void* obj, const wxCommandEvent& evt );
-	static void OnCorePluginStatusChanged( void* obj, const wxCommandEvent& evt );
-	static void OnSettingsApplied( void* obj, const int& evt );
-	static void OnSettingsLoadSave( void* obj, const IniInterface& evt );
+	static void __fastcall OnCoreThreadStatusChanged( void* obj, wxCommandEvent& evt );
+	static void __fastcall OnCorePluginStatusChanged( void* obj, wxCommandEvent& evt );
+	static void __fastcall OnSettingsApplied( void* obj, int& evt );
+	static void __fastcall OnSettingsLoadSave( void* obj, IniInterface& evt );
 
 	void LoadSaveRecentIsoList( IniInterface& conf );
 	void ApplySettings();

common/build/Utilities/Utilities.cbp
@@ -84,10 +84,10 @@
 		<Unit filename="../../../3rdparty/google/type_traits.h" />
 		<Unit filename="../../include/Utilities/Console.h" />
 		<Unit filename="../../include/Utilities/Dependencies.h" />
+		<Unit filename="../../include/Utilities/EventSource.h" />
 		<Unit filename="../../include/Utilities/Exceptions.h" />
 		<Unit filename="../../include/Utilities/General.h" />
 		<Unit filename="../../include/Utilities/HashMap.h" />
-		<Unit filename="../../include/Utilities/Listeners.h" />
 		<Unit filename="../../include/Utilities/MemcpyFast.h" />
 		<Unit filename="../../include/Utilities/Path.h" />
 		<Unit filename="../../include/Utilities/RedtapeWindows.h" />

common/build/Utilities/utilities.vcproj
@@ -426,6 +426,10 @@
 				>
 			</File>
 			<File
+				RelativePath="..\..\include\Utilities\EventSource.h"
+				>
+			</File>
+			<File
 				RelativePath="..\..\include\Utilities\Exceptions.h"
 				>
 			</File>
@@ -439,10 +443,6 @@
 			</File>
 			<File
 				RelativePath="..\..\include\intrin_x86.h"
-				>
-			</File>
-			<File
-				RelativePath="..\..\include\Utilities\Listeners.h"
 				>
 			</File>
 			<File

commit 0d80c0f5cadedf06c36cd0b6176be4cfa4685243

Attempt for the GCC 4.3 compilation errors introduced in r2063/2064, by selectively disabling __fastcall for the functions being used as listeners... hope it works. >_<

common/include/Utilities/EventSource.h
@@ -19,6 +19,15 @@
 
 class wxCommandEvent;
 
+// __evt_fastcall : Work-around for a GCC 4.3 compilation bug.  The templated FuncType
+// throws type mismatches if we have a __fastcall qualifier. >_< --air
+
+#if defined( __GNUC__ ) && (__GNUC__ < 4 ) || ((__GNUC__ == 4) && ( __GNUC_MINOR__ <= 3 ))
+#	define __evt_fastcall
+#else
+#	define __evt_fastcall
+#endif
+
 // --------------------------------------------------------------------------------------
 //  EventListener< typename EvtType >
 // --------------------------------------------------------------------------------------
@@ -26,7 +35,7 @@ class wxCommandEvent;
 template< typename EvtType >
 struct EventListener
 {
-	typedef void __fastcall FuncType( void* object, typename EvtType& evt );
+	typedef void __evt_fastcall FuncType( void* object, EvtType& evt );
 
 	void*		object;
 	FuncType*	OnEvent;

pcsx2/Memory.cpp
@@ -574,7 +574,7 @@ void memClearPageAddr(u32 vaddr)
 ///////////////////////////////////////////////////////////////////////////
 // PS2 Memory Init / Reset / Shutdown
 
-static void __fastcall mmap_OnPageFault( void* basemem, PageFaultInfo& info );
+static void __evt_fastcall mmap_OnPageFault( void* basemem, PageFaultInfo& info );
 
 static const uint m_allMemSize =
 		Ps2MemSize::Rom + Ps2MemSize::Rom1 + Ps2MemSize::Rom2 + Ps2MemSize::ERom +
@@ -894,7 +894,7 @@ static __forceinline void mmap_ClearCpuBlock( uint offset )
 	Cpu->Clear( m_PageProtectInfo[rampage].ReverseRamMap, 0x400 );
 }
 
-static void __fastcall mmap_OnPageFault( void* basemem, PageFaultInfo& info )
+static void __evt_fastcall mmap_OnPageFault( void* basemem, PageFaultInfo& info )
 {
 	// get bad virtual address
 	uptr offset = info.addr - (uptr)basemem;

pcsx2/RecoverySystem.cpp
@@ -34,7 +34,7 @@ int sys_resume_lock = 0;
 
 static FnType_OnThreadComplete* Callback_FreezeFinished = NULL;
 
-static void __fastcall StateThread_OnAppStatus( void* thr, AppEventType& stat )
+static void __evt_fastcall StateThread_OnAppStatus( void* thr, AppEventType& stat )
 {
 	if( (thr == NULL) || (stat != AppStatus_Exiting) ) return;
 	((PersistentThread*)thr)->Cancel();

pcsx2/gui/AppMain.cpp
@@ -250,7 +250,7 @@ void Pcsx2App::HandleEvent(wxEvtHandler *handler, wxEventFunction func, wxEvent&
 	}
 }
 
-static void __fastcall OnStateSaveFinished( void* obj, wxCommandEvent& evt )
+static void __evt_fastcall OnStateSaveFinished( void* obj, wxCommandEvent& evt )
 {
 	if( evt.GetInt() == CoreStatus_Resumed )
 	{

pcsx2/gui/MainFrame.cpp
@@ -255,28 +255,28 @@ void MainEmuFrame::InitLogBoxPosition( AppConfig::ConsoleLogOptions& conf )
 	}
 }
 
-void __fastcall MainEmuFrame::OnCoreThreadStatusChanged( void* obj, wxCommandEvent& evt )
+void __evt_fastcall MainEmuFrame::OnCoreThreadStatusChanged( void* obj, wxCommandEvent& evt )
 {
 	if( obj == NULL ) return;
 	MainEmuFrame* mframe = (MainEmuFrame*)obj;
 	mframe->ApplyCoreStatus();
 }
 
-void __fastcall MainEmuFrame::OnCorePluginStatusChanged( void* obj, wxCommandEvent& evt )
+void __evt_fastcall MainEmuFrame::OnCorePluginStatusChanged( void* obj, wxCommandEvent& evt )
 {
 	if( obj == NULL ) return;
 	MainEmuFrame* mframe = (MainEmuFrame*)obj;
 	mframe->ApplyCoreStatus();
 }
 
-void __fastcall MainEmuFrame::OnSettingsApplied( void* obj, int& evt )
+void __evt_fastcall MainEmuFrame::OnSettingsApplied( void* obj, int& evt )
 {
 	if( obj == NULL ) return;
 	MainEmuFrame* mframe = (MainEmuFrame*)obj;
 	mframe->ApplySettings();
 }
 
-void __fastcall MainEmuFrame::OnSettingsLoadSave( void* obj, IniInterface& evt )
+void __evt_fastcall MainEmuFrame::OnSettingsLoadSave( void* obj, IniInterface& evt )
 {
 	if( obj == NULL ) return;
 	MainEmuFrame* mframe = (MainEmuFrame*)obj;

pcsx2/gui/MainFrame.h
@@ -85,10 +85,10 @@ public:
 	void ReloadRecentLists();
 
 protected:
-	static void __fastcall OnCoreThreadStatusChanged( void* obj, wxCommandEvent& evt );
-	static void __fastcall OnCorePluginStatusChanged( void* obj, wxCommandEvent& evt );
-	static void __fastcall OnSettingsApplied( void* obj, int& evt );
-	static void __fastcall OnSettingsLoadSave( void* obj, IniInterface& evt );
+	static void __evt_fastcall OnCoreThreadStatusChanged( void* obj, wxCommandEvent& evt );
+	static void __evt_fastcall OnCorePluginStatusChanged( void* obj, wxCommandEvent& evt );
+	static void __evt_fastcall OnSettingsApplied( void* obj, int& evt );
+	static void __evt_fastcall OnSettingsLoadSave( void* obj, IniInterface& evt );
 
 	void LoadSaveRecentIsoList( IniInterface& conf );
 	void ApplySettings();